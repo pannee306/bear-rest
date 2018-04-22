9e474eb5b54559ac7e73daaa0a36e5e89dd7ec62
Pannee Sae-chee <punnee_ae@hotmail.com>
2018-04-23 00:28:34 +0700

    seperate actions

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,17 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+//import axios from 'axios';
 //import { createStore, applyMiddleware } from 'redux';
 import { createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 import reducers from './reducers';
+import { fetchBear } from './actions';
 
 
 const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
 
 let store = createStoreWithMiddleware(reducers)
 
-let fetchBearActionCreator = function(){
-  return (dispatch) => {
-    axios.get('http://localhost:8000/api/bears')
-    .then(result => {
-      dispatch({type: 'FETCH_BEAR', payload: result.data})
-    })
-  }
-}
+
 class App extends Component {
   constructor(props){
     super(props)
@@ -28,7 +22,7 @@ class App extends Component {
       
      this.setState({data: store.getState().bear})
     })
-    store.dispatch(fetchBearActionCreator());
+    store.dispatch(fetchBear());
   }
   render() {
     let bears = this.state.data;
